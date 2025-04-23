297b9563fd2c9435e3e8d7162acd032a610d96c5
edu-hc <edu.carvc@gmail.com>
2025-04-23 16:44:22 -0300

    feat: Exception handling

---
+++ src/main/java/com/abs/pagamentos/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.abs.pagamentos.controllers;
+
+import com.abs.pagamentos.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException e) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception e) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(e.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
---
+++ src/main/java/com/abs/pagamentos/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.abs.pagamentos.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
