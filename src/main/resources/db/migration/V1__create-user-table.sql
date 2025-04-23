107b73a2ded94d7f554483b8ee4c0851d1ea47e3
edu-hc <edu.carvc@gmail.com>
2025-04-23 16:42:14 -0300

    feat: flyway migrations PostgreSQL

---
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    document VARCHAR(30) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    balance NUMERIC(19, 2) NOT NULL,
+    user_type VARCHAR(30) NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__create_table_transactions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE transactions (
+    id BIGINT PRIMARY KEY,
+    amount NUMERIC(19, 2) NOT NULL,
+    sender_id BIGINT NOT NULL,
+    receiver_id BIGINT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    CONSTRAINT fk_sender FOREIGN KEY (sender_id) REFERENCES users(id),
+    CONSTRAINT fk_receiver FOREIGN KEY (receiver_id) REFERENCES users(id)
+);
